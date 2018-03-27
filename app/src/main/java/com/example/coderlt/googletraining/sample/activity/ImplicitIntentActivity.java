d9313da8e1a8d4ed455125ae577de3d77fcc6e9a
coderlt <coderliantang@gmail.com>
2018-03-27 12:11:27 +0800

    start activity for result

--- app/src/main/java/com/example/coderlt/googletraining/sample/activity/ImplicitIntentActivity.java
+++ app/src/main/java/com/example/coderlt/googletraining/sample/activity/ImplicitIntentActivity.java
@@ -3,26 +3,27 @@ package com.example.coderlt.googletraining.sample.activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.coderlt.googletraining.R;
+import com.example.coderlt.googletraining.sample.util.ToastUtil;
 
-import java.net.Proxy;
 import java.util.List;
-
-import static java.net.Proxy.Type.HTTP;
 
 public class ImplicitIntentActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = "ImplicitIntentActivity";
+    private static final int PICK_CONTACTS_REQUEST = 0x1111;
     private Button dailBtn;
     private Button emailBtn;
     private Button browserBtn;
     private Button mapsBtn;
+    private Button resultBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +33,13 @@ public class ImplicitIntentActivity extends AppCompatActivity implements View.On
         emailBtn = findViewById(R.id.email_btn);
         browserBtn = findViewById(R.id.browser_btn);
         mapsBtn = findViewById(R.id.maps_btn);
+        resultBtn = findViewById(R.id.start_for_result_btn);
 
         dailBtn.setOnClickListener(this);
         emailBtn.setOnClickListener(this);
         browserBtn.setOnClickListener(this);
         mapsBtn.setOnClickListener(this);
+        resultBtn.setOnClickListener(this);
     }
 
     @Override
@@ -78,8 +81,31 @@ public class ImplicitIntentActivity extends AppCompatActivity implements View.On
                 Uri mapsUri = Uri.parse("http://maps.google.com/maps?f=d&saddr=startLat%20startLng&daddr=endLat%20endLng&hl=en");
                 Intent mapsIntent = new Intent(Intent.ACTION_VIEW,mapsUri);
                 startActivity(mapsIntent);
+                break;
+            case R.id.start_for_result_btn:
+                Intent resultIntent=new Intent(Intent.ACTION_PICK,Uri.parse("content://contacts"));
+                resultIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+                startActivityForResult(resultIntent,PICK_CONTACTS_REQUEST);
+                break;
             default:
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        if(requestCode == PICK_CONTACTS_REQUEST){
+            if(resultCode == RESULT_OK){
+                Uri contactUri = data.getData();
+                // 提取需要的列 ,Cursor是一个 interface
+                String[] projections = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+                Cursor cursor = getContentResolver()
+                        .query(contactUri,projections,null,null,null);
+                cursor.moveToFirst();
+                String number = cursor.
+                        getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                ToastUtil.showToast("Number is : "+number);
+            }
         }
     }
 }
